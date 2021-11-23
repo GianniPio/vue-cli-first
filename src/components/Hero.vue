d9fc207afa9ce0a6deeded88dd9d8e156e596682
GianniPio <giannipiopellegrino00@gmail.com>
2021-11-23 16:51:00 +0100

    add new components

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,24 @@
 <template>
-    
+  <div class="jumbo">
+    <img src="https://mondogif.altervista.org/wp-content/uploads/2017/10/%E2%80%9CQuesta-%C3%A8-benzina-io-mi-d%C3%B2-fuoco%E2%80%9D-in-GIF-animata-2.gif" alt="Gif">
+  </div>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
-
-export default defineComponent({
-    setup() {
-        
-    },
-})
+export default {
+  name: "Jumbo",
+  props: {
+    msg: String,
+  },
+};
 </script>
 
-<style scoped>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    img {
+       
+       width: 50%;
+       height: 50%;
 
+    }
 </style>
